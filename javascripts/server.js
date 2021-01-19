f9dacb7ba1252464a0d0ae63b355307977a71b39
Khutso Baloyi <201284643@student.uj.ac.za>
2021-01-19 21:22:44 +0200

    first commit

---
+++ javascripts/server.js
@@ -0,0 +1,16 @@
+
+
+
+let btn = document.getElementById("send");
+
+btn.addEventListener("click", function() {
+    const emailParams = {
+        name: document.forms["myForm"]["name"].value,
+        email: document.forms["myForm"]["email"].value,
+        message: document.forms["myForm"]["message"].value
+    }
+
+    
+});
+
+
